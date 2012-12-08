d41105b0bbb3b7a9d8833e76f89538d812a3078a
liangbx <liangbx361@163.com>
2012-12-08 17:38:24 +0800

    增加地址搜寻功能， 删除功能

--- web/WebService/UserAddressInfo.asmx.cs
+++ web/WebService/UserAddressInfo.asmx.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
 
 using Cudo.Business;
 using Cudo.Entities;
@@ -31,6 +36,53 @@ namespace web.WebService
         {
             UserAddressBLL user = new UserAddressBLL();
             return user.GetList(userId);
+        }
+
+        /// <summary>
+        /// 删除地址
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [WebMethod]
+        public int deleteAddress(int id)
+        {
+            UserAddressBLL bll = new UserAddressBLL();
+            return bll.DeleteAddress(id);
+        }
+
+        /// <summary>
+        /// 获取区域
+        /// </summary>
+        /// <returns></returns>
+        [WebMethod]
+        public List<Area> getArea()
+        {
+            AreaBLL abll = new AreaBLL();
+            return abll.GetList(0);
+        }
+
+        /// <summary>
+        /// 获取街道
+        /// </summary>
+        /// <param name="aid"></param>
+        /// <returns></returns>
+        [WebMethod]
+        public List<Area> getStreet(string aid)
+        {
+            AreaBLL abll = new AreaBLL();
+            return abll.GetListByidlist(aid);
+        }
+
+        /// <summary>
+        /// 获取楼宇
+        /// </summary>
+        /// <param name="sid"></param>
+        /// <returns></returns>
+        [WebMethod]
+        public List<Area> getDistrict(string sid)
+        {
+            AreaBLL abll = new AreaBLL();
+            return abll.GetListByidlist(sid);
         }
     }
 }
